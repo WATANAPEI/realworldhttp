f13300c49a2609bd919c8d096a4380a6f34c7c99
WATANAPEI <info@wpei.dev>
2021-01-18 23:23:38 +0900

    upgrade

--- client_ch6/server/https_server.go
+++ client_ch6/server/https_server.go
@@ -3,9 +3,11 @@ package main
 import (
 	"crypto/tls"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -22,6 +24,41 @@ func highLevelApi() {
 	log.Println("start http listening: 18443")
 	err := http.ListenAndServeTLS(":18443", "./cert/server.crt", "./cert/server.key", nil)
 	log.Println(err)
+}
+
+func handlerUpgrade(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("Connection") != "Upgrade" || r.Header.Get("Upgrade") != "MyProtocol" {
+		w.WriteHeader(400)
+		return
+	}
+	fmt.Println("Upgrade to MyProtocol")
+
+	hijacker := w.(http.Hijacker)
+	conn, readWriter, err := hijacker.Hijack()
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	res := http.Response{
+		StatusCode: 101,
+		Header:     make(http.Header),
+	}
+	res.Header.Set("Upgrade", "MyProtocol")
+	res.Header.Set("Connection", "Upgrade")
+	res.Write(conn)
+
+	for i := 1; i <= 10; i++ {
+		fmt.Fprintf(readWriter, "%d\n", i)
+		fmt.Println("->", i)
+		readWriter.Flush()
+		recv, err := readWriter.ReadBytes('\n')
+		if err == io.EOF {
+			break
+		}
+		fmt.Printf("<- %s", string(recv))
+		time.Sleep(500 * time.Millisecond)
+	}
 }
 
 //test update
