d338a98d6af2053bcdaea138cfaa0e42f70326ee
WATANAPEI <info@wpei.dev>
2021-01-24 23:58:28 +0900

    save server-push

--- client_ch9/server/server.go
+++ client_ch9/server/server.go
@@ -3,10 +3,55 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/big"
 	"net/http"
+	"time"
 )
 
 var image []byte
+var html []byte
+
+// send HTML to browser
+func handlerHtml(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html")
+	w.Write(html)
+}
+
+func handlerPrimeSSE(w http.ResponseWriter, r *http.Request) {
+	flusher, ok := w.(http.Pusher)
+	if !ok {
+		http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
+		return
+	}
+	ctx := r.Context()
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	var num int64 = 1
+	fo id := 1; id <= 100; id++ {
+		select {
+		case <- ctx.Done():
+			fmt.Println("connection closed from client")
+			return
+		default:
+			//do noting
+		}
+		for {
+			num++
+			if big.NewInt(num).ProbablyPrime(20) {
+				fmt.Println(num)
+				fmt.Fprintf(w, "data: {\"id\": %d, \"number\": %d}\n\n", id, num)
+				flusher.Flush()
+				time.Sleep(time.Second)
+				break
+			}
+		}
+		time.Sleep(time.Second)
+	}
+	fmt.Println("Connection colsed from server")
+}
 
 func init() {
 	var err error
@@ -31,9 +76,19 @@ func handlerImage(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// http.HandleFunc("/", handlerHtml)
+	// http.HandleFunc("/image", handlerImage)
+	// fmt.Println("start http listening :18443")
+	// err := http.ListenAndServeTLS(":18443", "./cert/server.crt", "./cert/server.key", nil)
+	// fmt.Println(err)
+	var err error
+	html, err := ioutil.ReadFile("index.html")
+	if err!= nil {
+		panic(err)
+	}
 	http.HandleFunc("/", handlerHtml)
-	http.HandleFunc("/image", handlerImage)
-	fmt.Println("start http listening :18443")
-	err := http.ListenAndServeTLS(":18443", "./cert/server.crt", "./cert/server.key", nil)
+	http.HandleFunc("/prime", handlerPrimeSSE)
+	fmt.Println("start http listening: 18888")
+	err = http.ListenAndServe(":18888", nil)
 	fmt.Println(err)
 }
